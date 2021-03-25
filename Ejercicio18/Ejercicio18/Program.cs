daf96b5d8a0acb0b82bc76438d79f59392e06edf
diego.taub <diego.taub@accenture.com>
2021-03-25 20:59:26 -0300

    Ejercicio18

---
+++ Ejercicio18/Ejercicio18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número: ");
+            if (!double.TryParse(Console.ReadLine(), out double num1))
+            {
+                Console.Write("Debe ingresar un número. Pulse una tecla para salir.");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.Write($"El número ingresado redondeado a dos decimales es: {Math.Round(num1, 2)}");
+            Console.ReadKey();
+        }
+    }
+}
